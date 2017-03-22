e3325164c95e4366d98653fe827df8b623bf7881
SorBlackPlus <sorblackplus@gmail.com>
2017-03-22 02:41:55 -0700

    @sorblack :)

--- manual-creator.lua
+++ manual-creator.lua
@@ -9,4 +9,3 @@ io.open("tabchi-"..last..".sh",'w'):write("while true; do\n./telegram-cli-1222 -
 io.popen("chmod 777 tabchi-"..last..".sh")
 redis:set('tabchi:'..last..':fullsudo',sudo)
 print("Done!\nNew Tabchi Created...\nID : "..last.."\nFull Sudo : "..sudo.."\nRun : ./tabchi-"..last..".sh")
-
